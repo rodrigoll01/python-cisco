d9e2cb26ba7c0f7360e8c95fbe4acfd8b53e2c70
Rodrigo Lima Cavalcante <cavalcanterodrigolima@gmail.com>
2023-10-31 11:02:29 -0300

    Operações com variáveis

---
+++ variaveis.py
@@ -0,0 +1,56 @@
+var = 1
+account_balance = 1000.0
+client_name = 'John Doe'
+print(var, account_balance, client_name)
+print(var, "\n")
+
+var = "3.12.0"
+print("Python version: " + var, "\n")
+
+var = 1
+print(var)
+var = var + 1
+print(var, "\n")
+
+var = 100
+var = 200 + 300
+print(var, "\n")
+
+a = 3.0
+b = 4.0
+c = (a ** 2 + b ** 2) ** 0.5
+print("c =", c, "\n")
+
+john = 3
+mary = 5
+adam = 6
+print(f"John tem {john} maçãs, Mary tem {mary} maçãs, e Adam tem {adam} maçãs")
+total_apples = john + mary + adam
+print("A soma das maçãs de John, Mary e Adam é: ", total_apples, "\n")
+
+x =2
+x *= 2
+print(x, "\n")
+
+sheep = 1
+sheep += 1
+print(sheep, "\n")
+
+print("i = i + 2 * j ⇒ i += 2 * j", "\n")
+print("var = var / 2 ⇒ var /= 2", "\n")
+print("rem = rem % 10 ⇒ rem %= 10", "\n")
+print("j = j - (i + var + rem) ⇒ j -= (i + var + rem)", "\n")
+print("x = x ** 2 ⇒ x **= 2", "\n")
+
+kilometers = 12.25
+miles = 7.38
+miles_to_kilometers = miles * 1.61
+kilometers_to_miles = kilometers / 1.61
+print(miles, "milhas equivale a", round(miles_to_kilometers, 2), "kilometros")
+print(kilometers, "kilometros equivale a", round(kilometers_to_miles, 2), "milhas", "\n")
+
+x =  0
+x = float(x)
+y = (3 * (x**3) - 2 * (x**2) + (3*x) - 1)
+print("y =", y)
+
