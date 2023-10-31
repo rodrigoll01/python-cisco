83cfabeb7d459b8de02994e5771c67290e714ea9
Rodrigo Lima Cavalcante <cavalcanterodrigolima@gmail.com>
2023-10-31 19:53:13 -0300

    Adição de novos códigos

--- variaveis.py
+++ variaveis.py
@@ -52,5 +52,10 @@ print(kilometers, "kilometros equivale a", round(kilometers_to_miles, 2), "milha
 x =  0
 x = float(x)
 y = (3 * (x**3) - 2 * (x**2) + (3*x) - 1)
-print("y =", y)
+print("y =", y, "\n")
+
+a = 6
+b = 3
+a /= 2 * b
+print(a)
 
